da9d7321b193e30a2db6bdd94896cc875db9290f
jeramy <jzapotosky@lcad.edu>
2021-10-22 11:30:51 -0700

    More UI Work

--- Assets/Scripts/ToyBox/TitleController.cs
+++ Assets/Scripts/ToyBox/TitleController.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class TitleController : MonoBehaviour
 {
     public GameObject mainMenu = null;
     public GameObject optionsMenu = null;
 
+    public Slider _volumeSlider = null;
+    public Toggle _vsyncToggle = null;
+    public TMP_Dropdown _dropDown = null;
+
+    public float _volume = 1;
+    public bool _vsync = true;
+    public int _languageSelected = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _vsyncToggle.isOn = _vsync;
+        _volumeSlider.value = _volume;
+        _dropDown.value = _languageSelected;
+
+        _vsyncToggle.onValueChanged.AddListener(OnToggle_Vsync);
+        _volumeSlider.onValueChanged.AddListener(OnValueChanged_Volume);
+        _dropDown.onValueChanged.AddListener(OnDropdown_Language);
     }
 
     // Update is called once per frame
@@ -37,5 +53,26 @@ public class TitleController : MonoBehaviour
     {
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+    }
+
+    public void OnToggle_Vsync(bool value)
+    {
+        _vsync = value;
+
+        Debug.LogError("Vsync: " + value);
+    }
+
+    public void OnValueChanged_Volume(float volume)
+    {
+        _volume = volume;
+
+        Debug.LogError("Volume: " + volume);
+    }
+
+    public void OnDropdown_Language(int index)
+    {
+        _languageSelected = index;
+
+        Debug.LogError("Language: " + _dropDown.options[index].text);
     }
 }
