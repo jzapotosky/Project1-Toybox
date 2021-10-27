1da3ac909331b3db1c136e7af434ed44f27307e7
jeramy <jzapotosky@lcad.edu>
2021-10-27 15:57:23 -0700

    Work In Progress Tic Tac Toe

--- Assets/Scripts/TicTacToe/TicTacToe.cs
+++ Assets/Scripts/TicTacToe/TicTacToe.cs
@@ -16,10 +16,24 @@ public class TicTacToe : MonoBehaviour
     private bool releaseWait = false;
     private Mouse mouse = null;
 
+    //UI
+    public GameObject xTurnUI = null;
+    public GameObject oTurnUI = null;
+
+    public GameObject xWinUI = null;
+    public GameObject oWinUI = null;
+
+    public GameObject xPrefab = null;
+    public GameObject oPrefab = null;
+
+    public GameObject resetButton = null;
+
     // Start is called before the first frame update
     void Start()
     {
         mouse = Mouse.current;
+
+        UpdateTurnUI();
     }
 
     // Update is called once per frame
@@ -35,7 +49,70 @@ public class TicTacToe : MonoBehaviour
             releaseWait = false;
 
             //After Mouse Click
+            Ray ray = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Tile hitTile = hit.transform.GetComponent<Tile>();
+
+                if(hitTile != null && CheckTileOpen(hitTile.row, hitTile.col))
+                {
+                    if(xTurn)
+                    {
+                        Instantiate(xPrefab, hitTile.transform.position, Quaternion.identity);
+                        SetTile(hitTile.row, hitTile.col);
+                    }
+                    else
+                    {
+                        Instantiate(oPrefab, hitTile.transform.position, Quaternion.identity);
+                        SetTile(hitTile.row, hitTile.col);
+                    }
+
+                    if (CheckWin(hitTile.row, hitTile.col))
+                    {
+                        if (xTurn)
+                        {
+                            xTurnUI.SetActive(false);
+                            xWinUI.SetActive(true);
+                        }
+                        else
+                        {
+                            oTurnUI.SetActive(false);
+                            oWinUI.SetActive(true);
+                        }
+
+                        resetButton.SetActive(true);
+                    }
+                    else
+                    {
+                        xTurn = !xTurn;
+                        UpdateTurnUI();
+                    }
+                }
+            }
         }
+    }
+
+    public void UpdateTurnUI()
+    {
+        if(xTurn)
+        {
+            xTurnUI.SetActive(true);
+            oTurnUI.SetActive(false);
+        }
+        else
+        {
+            xTurnUI.SetActive(false);
+            oTurnUI.SetActive(true);
+        }
+    }
+
+    public void OnClick_Reset()
+    {
+        //Turn off the Win UI
+        //Destroy all of the piece gameobjects
+        //Clear tiles
+        //turn off reset button
     }
 
     private void SetTile(int row, int col)
